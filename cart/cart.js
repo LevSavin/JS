d55e9c1e5585db607b666c654bb34949b4ae92e4
LevSavin <skif-sw@mail.ru>
2021-03-20 19:52:58 +0300

    cart oojs

--- cart/cart.js
+++ cart/cart.js
@@ -1,104 +1,274 @@
-var NewItem = function (name, imgSrc, price, id) {
-    this.name = name;
-    this.imgSrc = imgSrc;
-    this.price = price;
-    this.id = id;
-};
+class Api {
+    constructor() {
+        this.url = "./goods.json";
+    }
 
-var i0 = new NewItem("Леггинсы", "img/1.jpg", 3000, 0);
-var i1 = new NewItem("Платье", "img/2.jpg", 4000, 1);
-var i2 = new NewItem("Свитер", "img/3.jpg", 5000, 2);
+    fetch(error, success) {
+        let xhr;
 
-var catalogArray = [i0, i1, i2];
-var cartArray = []
-var sum = 0;
-var selectedItem, indexOfname;
-var cartItem = {};
+        if (window.XMLHttpRequest) {
+            xhr = new XMLHttpRequest();
+        } else if (window.ActiveXObject) {
+            xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        }
 
-function init() { // отрисовка каталога
-    let $catalog = document.querySelector(".catalog");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    success(JSON.parse(xhr.responseText));
+                } else if (xhr.status > 400) {
+                    error("Ошибка");
+                }
+            }
+        }
 
-    const renderGoodsItem = ({
-        name,
-        imgSrc,
-        price,
-        id
-    }) => {
-        return `<div class="div__item"><img src=${imgSrc} class="div__img"><div class="div__item_goods-content">${name} ${price} \u20bd<button class="div__button" id="btn_${id}" onclick="addItem(event)">Добавить</button></div></div>`;
+        xhr.open('GET', this.url, true);
+        xhr.send();
+    }
+
+    fetchPromise() {
+        return new Promise((resolve, reject) => {
+            this.fetch(reject, resolve)
+        })
+    }
+}
+
+class GoodsItem {
+    constructor(title, price, src, id) {
+        this.title = title;
+        this.src = src;
+        this.price = price;
+        this.id = id;
+    }
+
+    getHtml() {
+        return `<div class="div__item"><img src=${this.src} class="div__img"><div class="div__item_goods-content">${this.title} ${this.price} \u20bd<button class="div__button" id="btn_${this.id}">Добавить</button></div></div>`;
+    }
+}
+
+class Buttons {
+    constructor() {
+        this.$buttonsAdd = document.querySelector(".catalog");
+        this.$search = document.querySelector('#search');
+    }
+
+    setButton() {
+        this.$buttonsAdd.addEventListener("click", event => {
+            var button = event.target.closest("button");
+            if (button) { //проверяем, вдруг button-родителя нет
+                goodsList.addToCart(button)
+            }
+        });
+    }
+
+    setSearchHandler(callback) {
+        this.$search.addEventListener('input', callback);
+    }
+}
+
+class GoodsList {
+    constructor() {
+        this.api = new Api();
+        this.$goodsList = document.querySelector(".catalog");
+        this.goods = [];
+        this.filteredGoods = [];
+
+        const fetch = this.api.fetchPromise();
+
+        fetch.then((data) => {
+                this.onFetchSuccess(data)
+            })
+            .catch((err) => {
+                this.onFetchError(err)
+            });
+    }
+
+    onFetchSuccess(data) {
+        this.goods = data.map(({
+            title,
+            price,
+            src,
+            id
+        }) => new GoodsItem(title, price, src, id));
+        this.filteredGoods = this.goods;
+        this.render();
+        this.addBtn();
+    }
+
+    onFetchError(err) {
+        this.$goodsList.insertAdjacentHTML("beforeend", `<h3>${err}</h3>`);
+    }
+
+    render() {
+        this.$goodsList.textContent = "";
+        this.filteredGoods.forEach((item) => {
+            this.$goodsList.insertAdjacentHTML('beforeend', item.getHtml());
+        })
+    }
+
+    addBtn() {
+        this.buttons = new Buttons();
+        this.buttons.setButton();
+        this.buttons.setSearchHandler((evt) => {
+            this.search(evt.target.value);
+        })
+
+    }
+
+    addToCart(button) {
+        cartList.addToCart(button)
+    }
+
+    search(str) {
+        if (str === '') {
+            this.filteredGoods = this.goods;
+        }
+        const regexp = new RegExp(str, 'gi');
+        this.filteredGoods = this.goods.filter((good) => regexp.test(good.title));
+        this.render();
+    }
+}
+
+class CartItem {
+    constructor(title, price, src, id, quantity) {
+        this.title = title;
+        this.src = src;
+        this.price = price;
+        this.id = id;
+        this.quantity = quantity;
+    }
+
+    getHtml() {
+        return `<tbody id="itemCart_${this.id}"><tr><td>${this.title} </td><td id="quantity_${this.id}">${this.quantity}</td><td> шт.</td><td id="price_${this.id}">${this.price*this.quantity}</td><td>\u20bd</td><td><button data-action="increase" id="increase_${this.id}">+</button><td><button data-action="decrease" id="decrease_${this.id}">-</button></td></tr></tbody>`;
+    }
+}
+
+class CartButtons {
+    constructor(elem) {
+        elem.onclick = this.onClick.bind(this); //  метод this.onClick привязывается к контексту текущего объекта this. Т.к. иначе this внутри него будет ссылаться на DOM-элемент (elem), а не на объект Buttons, и this[action] будет не тем, что нам нужно.
+    }
+
+    increase(button) {
+        cartList.increase(button)
+    }
+
+    decrease(button) {
+        cartList.decrease(button)
+    }
+
+    clearCart() {
+        cartList.clearCart()
+    }
+
+    onClick(event) {
+        let button = event.target.closest("button");
+        if (button) { //проверяем, вдруг button-родителя нет
+            let action = event.target.closest("button").dataset.action; // определяем значения атрибута data у кнопки
+            if (action) {
+                this[action](button);
+            }
+        }
     };
+}
 
-    const renderGoodsList = (list = []) => { 
-        let goodsList = list.map(renderGoodsItem).join("");
-        $catalog.insertAdjacentHTML('beforeend', goodsList);
+class CartList {
+    constructor() {
+        this.$cartList = document.querySelector(".selected-items");
+        this.$cartSum = document.querySelector(".sum");
+        this.cartArray = [];
+        this.fetchGoods()
     }
 
-    renderGoodsList();
-}
-
-function addItem(obj) { // добавить товар в корзину
-    itemNum = obj.target.id.split("_")[1] //индекс товара из массива товаров в каталоге
-    selectedItem = catalogArray[itemNum];
-
-    if (hasAlready() == undefined) { // если товара нет в корзине, то добавить его
-        cartItem = new NewItem(selectedItem.name, selectedItem.imgSrc, selectedItem.price, selectedItem.id);
-        cartItem.quantity = 1;
-        cartArray.push(cartItem);
-
-        const renderCartItem = ({
-            name,
-            quantity,
-            price
-        }) => {
-            return `<tbody id="itemString_${itemNum}"><tr><td id="itemQuantity_${itemNum}">${name} ${quantity} шт.</td><td id="price${itemNum}">${price}\u20bd</td><td><button id="removeBtn_${itemNum}" onclick="removeItem(event)">-</button></td></tr></tbody>`;
-        };
-        let $selectedItems = document.querySelector(".selected-items"); //корзина в html
-        $selectedItems.insertAdjacentHTML('beforeend', renderCartItem(cartItem));
-    } else {
-        indexOfname = cartArray.findIndex(x => x.id === catalogArray[itemNum].id); // по id ищем в корзине товар, который нужно добавить из каталога
-        cartArray[indexOfname].quantity += 1; // увеличить количество
-        changePrice()
+    fetchGoods() {
+        if (this.cartArray.length > 0) {
+            this.cartArray = this.cartArray.map(({
+                title,
+                price,
+                src,
+                id,
+                quantity
+            }) => new CartItem(title, price, src, id, quantity));
+            this.render();
+            this.countSum();
+            this.addBtn();
+        }
     }
-    countSum();
-}
 
-function hasAlready() { // проверяем по id, есть ли уже в корзине выбранный товар
-    return cartArray.find(x => x.id === selectedItem.id);
-}
-
-function removeItem(obj) { // уменьшить количество товара / скрыть позицию
-    itemNum = obj.target.id.split("_")[1] // индекс товара из массива товаров в каталоге
-    indexOfname = cartArray.findIndex(x => x.id === catalogArray[itemNum].id); // индекс товара из массива корзины
-    if (cartArray[indexOfname].quantity > 1) {
-        cartArray[indexOfname].quantity -= 1; // уменьшить количество
-        changePrice()
-    } else {
-        let remItem = document.getElementById("itemString_" + itemNum)
-        remItem.remove()
-        cartArray.splice(indexOfname, 1)
+    render() {
+        this.$cartList.textContent = "";
+        this.cartArray.forEach((good) => {
+            this.$cartList.insertAdjacentHTML('beforeend', good.getHtml());
+        })
     }
-    countSum()
+
+    addBtn() {
+        this.buttons = new CartButtons(DOMcartList);
+    }
+
+    countSum() {
+        let initialValue = 0;
+        let sum = this.cartArray.reduce(function (accumulator, currentValue) {
+            return accumulator + (currentValue.price * currentValue.quantity);
+        }, initialValue);
+        document.querySelector(".sum").textContent = `${sum} \u20bd`;
+        if (this.cartArray.length < 1) {
+            this.$cartSum.textContent = "";
+        }
+    }
+
+    setId(button) {
+        catalogId = button.id.split("_")[1] //индекс товара из массива товаров в каталоге
+        cartId = this.cartArray.findIndex(x => x.id == catalogId); // индекс товара из массива корзины
+    }
+
+    addToCart(button) {
+        let itemNum = button.id.split("_")[1] //индекс товара из массива товаров в каталоге
+        let selectedItem = goodsList.goods[itemNum];
+
+        if ((this.cartArray.length < 1) || (hasAlready(this.cartArray) == undefined)) {
+            let cartItem = Object.create(selectedItem)
+            cartItem.quantity = 1;
+            this.cartArray.push(cartItem);
+            this.fetchGoods()
+        } else {
+            this.increase(button)
+        }
+
+        function hasAlready(cart) { // проверяем по id, есть ли уже в корзине выбранный товар
+            return cart.find(x => x.id === selectedItem.id);
+        }
+    }
+
+    increase(button) {
+        this.setId(button);
+        this.cartArray[cartId].quantity += 1;
+        document.getElementById("quantity_" + catalogId).textContent = this.cartArray[cartId].quantity;
+        document.getElementById("price_" + catalogId).textContent = this.cartArray[cartId].quantity * this.cartArray[cartId].price;
+        this.countSum()
+    }
+
+    decrease(button) {
+        this.setId(button);
+        if (this.cartArray[cartId].quantity > 1) {
+            this.cartArray[cartId].quantity -= 1;
+            document.getElementById("quantity_" + catalogId).textContent = this.cartArray[cartId].quantity;
+            document.getElementById("price_" + catalogId).textContent = this.cartArray[cartId].quantity * this.cartArray[cartId].price;
+        } else {
+            let remItem = document.getElementById("itemCart_" + catalogId)
+            remItem.remove()
+            this.cartArray.splice(cartId, 1)
+        }
+        this.countSum()
+    }
+
+    clearCart() {
+        this.cartArray = [];
+        this.countSum()
+        this.$cartList.textContent = "";
+        this.$cartSum.textContent = "";
+    }
 }
 
-function changePrice() { // изменить итог стоимости позиции
-    let changePrice = document.getElementById("price" + itemNum);
-    changePrice.textContent = ((cartArray[indexOfname].price * cartArray[indexOfname].quantity) + "\u20bd");
-    let changeQuantity = document.getElementById("itemQuantity_" + itemNum);
-    changeQuantity.textContent = (cartArray[indexOfname].name + " " + cartArray[indexOfname].quantity + " шт.");
-}
-
-function countSum() { // считаем итоговую сумму корзины
-    let initialValue = 0; // объект, используемый в качестве первого аргумента при первом вызове функции callback
-    sum = cartArray.reduce(function (accumulator, currentValue) { // подсчёт суммы по товарам в корзине
-        return accumulator + (currentValue.price * currentValue.quantity);
-    }, initialValue);
-    document.querySelector(".sum").textContent = "Сумма: " + sum + "\u20bd";
-}
-
-function clearCart() { // очистка корзины     
-    cartArray = [];
-    sum = 0;
-    document.querySelector(".sum").textContent = "Сумма: " + sum + "\u20bd";
-    document.querySelector(".selected-items").textContent = '';
-}
-
-window.onload = init;
+var catalogId, cartId;
+const goodsList = new GoodsList();
+const cartList = new CartList();
